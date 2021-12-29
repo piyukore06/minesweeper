77263cc693e282d8cbc6f3977460c3cad07852cb
shete <priyanka.shete@adesso.de>
2021-12-29 11:40:03 +0100

    Loading smalled game on smaller screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import Menu from './Components/Menu';
 import Octocat from './Components/Octocat';
 import Playground from './Components/Playground';
 import { Games } from './Constants/Game';
+import useWindowDimensions from './Hooks/useWindowDimentions';
 
 export const GameContext = React.createContext(Games.find((game) => game.id === 'beginner'));
 GameContext.displayName = 'GameContext';
 
 function App() {
-  const [gameType, setGameType] = useState(Games[0]);
+  const { width } = useWindowDimensions();
+  const [gameType, setGameType] = useState(width < 600 ? Games[0] : Games[1]);
   const [gameState, setGameState] = useState('🙂');
+
   return (
     <div style={{ backgroundImage: `url(/images/bluemoon.png)` }} className="body">
       <Octocat />
